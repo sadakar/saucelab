6f10af637591fc79f02fd3154ba7d5aca7737fca
Sadakar Pochampalli <sadakar.1988@gmail.com>
2024-08-03 16:29:13 +0530

    Create src/main/java/pagefactory/HomePageActions.java

---
+++ src/main/java/pagefactory/HomePageActions.java
@@ -0,0 +1,10 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePageActions {
+    
+    private WebDriver driver;
+    
+    @FindBy(id = \
