5b07f8b23cb30018b224e8e37ab20ad3d1dd0b78
sadakar_pochampalli <sadakar_pochampalli@epam.com>
2024-08-03 16:23:46 +0530

    updated file name

--- src/main/java/pagefactory/MenuPage.java
+++ src/main/java/pagefactory/MenuPage.java
@@ -5,13 +5,13 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class ProductsPage {
+public class MenuPage {
     WebDriver driver;
 
     @FindBy(id = "//*[@id='header_container']/div[2]/span")
     WebElement productsLabel;
 
-    public ProductsPage(WebDriver driver) {
+    public MenuPage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
